c4c1accb8c3508a93bf650109efce05c3a76acc1
Lino <glino.jardim@gmail.com>
2024-06-09 11:50:22 +0200

    Added the functionality to retrieve data if the button is clicked

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
         button = QPushButton("Press Me!")
-
-        self.setFixedSize(QSize(400, 300))
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
 
+    def the_button_was_clicked(self):
+        print("Clicked")
+        
+    def the_button_was_toggled(self, checked):
+        print("Checked?", checked)
 
 app = QApplication(sys.argv)
 
